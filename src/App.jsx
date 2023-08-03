82e20d4b3854b489dd813e0b786ffca7e409c862
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-08-03 18:20:59 -0500

    Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,35 @@
+import { useState } from 'react';
+
+import data from './data';
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+
+  console.log(count);
+
+  return (
+    <section className="section-center">
+      <h4>tired of boring lorem ipsum?</h4>
+      <form className="lorem-form">
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          min="1"
+          step="1"
+          max="9"
+          id="amount"
+          name="amount"
+          value={count}
+          onChange={(event) => {
+            setCount(event.target.value);
+          }}
+        />
+        <button className="btn" type="submit">
+          generate
+        </button>
+      </form>
+    </section>
+  );
 };
 export default App;
